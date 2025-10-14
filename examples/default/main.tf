0d156b06a8ae6708a01b71dda31818b98c67d4c2
Dennis Hoppe <dennis.hoppe@dhoppe.it>
2025-10-14 15:05:12 +0200

    docs(terraform): Describe example in more detail

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,7 +1,14 @@
 module "hcloud_rdns" {
   source = "../../"
 
+  for_each = toset(
+    [
+      data.hcloud_server.this.ipv4_address,
+      data.hcloud_server.this.ipv6_address
+    ]
+  )
+
   server_id  = data.hcloud_server.this.id
-  ip_address = data.hcloud_server.this.ipv4_address
+  ip_address = each.value
   dns_ptr    = "example-server.dhoppe.it"
 }
